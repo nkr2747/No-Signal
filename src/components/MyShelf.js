0f07da80629996cf7a1df3824dcfb4373629d911
MohitRajSS <sutravemohitraj@gmail.com>
2024-07-10 15:14:43 +0530

    MyShelf updated {container added}

--- src/components/MyShelf.js
+++ src/components/MyShelf.js
@@ -2,21 +2,21 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 export default function MyShelf() {
-  const arr = [1, 2, 3,4];
+  const arr = [1, 2, 3, 4];
   const requestedbk = [1, 2];
-  const pendingbk = [1, 2,5];
-  const lup=[1,2,3];
-  function LatestUpdates(x){
+  const pendingbk = [1, 2, 5];
+  const lup = [1, 2, 3];
+  function LatestUpdates(x) {
     return (
       <div className="container">
         <p>Maintenance break till submission</p>
         <hr />
       </div>
-      
+
     )
   }
-  function funrequestedbk(x){
-    return(
+  function funrequestedbk(x) {
+    return (
       <div className="container ">
         <div className="row">
           <div className="col-4 mx-2">
@@ -26,7 +26,7 @@ export default function MyShelf() {
             Title of book
             <p className="small my-2">Author</p>
             <button type="button" class="btn btn-outline-secondary btn-sm my-4">Unavailable</button>
-            
+
           </div>
           <hr />
         </div>
@@ -34,8 +34,8 @@ export default function MyShelf() {
       </div>
     )
   }
-  function funpendingbk(x){
-    return(
+  function funpendingbk(x) {
+    return (
       <div className="container ">
         <div className="row">
           <div className="col-4 mx-2">
@@ -45,7 +45,7 @@ export default function MyShelf() {
             Title of book
             <p className="small my-2">Author</p>
             <button type="button" class="btn btn-outline-danger btn-sm my-4">Return</button>
-            
+
           </div>
           <hr />
         </div>
@@ -56,8 +56,8 @@ export default function MyShelf() {
   function shelfBooks(x) {
     return (
       <div class="card rounded border border-0 mx-3 " style={{
-        width:"15rem",
-        height:"15rem"
+        width: "15rem",
+        height: "15rem"
       }}>
         <div class="card-body">This is some text within a card body.</div>
       </div>
@@ -65,100 +65,111 @@ export default function MyShelf() {
   }
   return (
     <>
-    <SearchBar/>
-    <div className="container border py-3">
-      <div className="container align-items-start border my-2">
-        <h5>
-          My{" "}
-          <span
-            style={{
-              color: "red",
-            }}
-          >
-            Shelf
-          </span>
-        </h5>
-      </div>
-      <div className="container border my-2">
-        <ul class="nav justify-content-between">
-          <li class="nav-item">
-            <a
-              class="nav-link link-secondary active"
-              aria-current="page"
-              href="/"
+      <SearchBar />
+      <div className="container border py-3">
+        <div className="container align-items-start border my-2">
+          <h5>
+            My{" "}
+            <span
+              style={{
+                color: "red",
+              }}
             >
-              Favourite
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link link-secondary" href="/">
-              Borrowed Books
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link link-secondary" href="/">
-              E-Books
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link link-secondary" href="/">
-              Audio Books
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link link-secondary" href="/">
-              Articles & Journals
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="row py-2 g-5 my-2 justify-content-center  border">
-      {
+              Shelf
+            </span>
+          </h5>
+        </div>
+        <div className="container border my-2 d-flex justify-content-between align-items-center" style={{ height: '50px' }}>
+          <p className="mb-0">
+            <strong>Name:</strong> Nilesh
+          </p>
+          <p className="mb-0">
+            <strong>Branch:</strong> Computer Science
+          </p>
+          <p className="mb-0">
+            <strong>Department:</strong> Engineering
+          </p>
+        </div>
+        <div className="container border my-2">
+          <ul class="nav justify-content-between">
+            <li class="nav-item">
+              <a
+                class="nav-link link-secondary active"
+                aria-current="page"
+                href="/"
+              >
+                Favourite
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link-secondary" href="/">
+                Borrowed Books
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link-secondary" href="/">
+                E-Books
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link-secondary" href="/">
+                Audio Books
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link link-secondary" href="/">
+                Articles & Journals
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="row py-2 g-5 my-2 justify-content-center  border">
+          {
             arr.map(shelfBooks)
-        }
-      </div>
-      <div className="container ">
-        <h4 className="my-4">Reminders</h4>
-        <div className="row gx-3">
-          <div className="col  p-3 rounded mx-2 align-items-stretch" style={{
-            backgroundColor:'white'
-          }}> 
-          <div className="container">
-          <h6>Latest Updates</h6> 
-          </div>
-          <div className="vstack gap-3">
-          {
-            lup.map(LatestUpdates)
           }
-          </div>
-          </div>
-          <div className="col  p-3  rounded mx-2 align-items-stretch" style={{
-            backgroundColor:'white'
-          }}> 
-          <div className="container my-2 ">
-          <h6>Pending Books</h6></div> 
-          <div className="vstack gap-3">
-          {
-            pendingbk.map(funpendingbk)
-          }
-          </div>
-          
-          </div>
-          <div className="col p-3 rounded mx-2 align-items-stretch" style={{
-            backgroundColor:'white'
-          }}> 
-          <div className="container my-2">
-          <h6>Requested Books</h6> 
-          </div>
-          <div className="vstack gap-3">
-          {
-            requestedbk.map(funrequestedbk)
-          }
-          </div>
+        </div>
+        <div className="container ">
+          <h4 className="my-4">Reminders</h4>
+          <div className="row gx-3">
+            <div className="col  p-3 rounded mx-2 align-items-stretch" style={{
+              backgroundColor: 'white'
+            }}>
+              <div className="container">
+                <h6>Latest Updates</h6>
+              </div>
+              <div className="vstack gap-3">
+                {
+                  lup.map(LatestUpdates)
+                }
+              </div>
+            </div>
+            <div className="col  p-3  rounded mx-2 align-items-stretch" style={{
+              backgroundColor: 'white'
+            }}>
+              <div className="container my-2 ">
+                <h6>Pending Books</h6></div>
+              <div className="vstack gap-3">
+                {
+                  pendingbk.map(funpendingbk)
+                }
+              </div>
+
+            </div>
+            <div className="col p-3 rounded mx-2 align-items-stretch" style={{
+              backgroundColor: 'white'
+            }}>
+              <div className="container my-2">
+                <h6>Requested Books</h6>
+              </div>
+              <div className="vstack gap-3">
+                {
+                  requestedbk.map(funrequestedbk)
+                }
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
