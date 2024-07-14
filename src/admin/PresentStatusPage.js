a205e564bcc8efd7b6110737b03c18974f4f0ccc
MohitRajSS <sutravemohitraj@gmail.com>
2024-07-14 15:27:41 +0530

    Updated PresentStatusPage.js

--- src/admin/PresentStatusPage.js
+++ src/admin/PresentStatusPage.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "../components/Support.css";
+// import "../components/Support.css";
 
 const userData = {
   name: "John Doe",
   branch: "Computer Science",
   program: "B.Tech",
 };
+
 const statusStyle = {
   color: "red",
 };
@@ -13,6 +14,7 @@ const statusStyle = {
 export default function PresentStatusPage() {
   return (
     <>
+    <h1>Present <span style={statusStyle}>Status</span>of Student</h1>
       <div
         className="container rounded my-2 p-3 d-flex justify-content-between align-items-center"
         style={{ backgroundColor: "white" }}
@@ -27,56 +29,153 @@ export default function PresentStatusPage() {
           <strong>Department:</strong> {userData.program}
         </p>
       </div>
-      {/* // */}
-      <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3">
-        <ul class="nav justify-content-between text-center w-100">
-          <li class="nav-item ">
-            <a class="nav-link link-secondary" href="#scrollspyHeading1">
-              Favourites
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link link-secondary" href="#scrollspyHeading2">
-              Borrowed
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link link-secondary" href="#scrollspyHeading3">
-              Requested
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link link-secondary" href="#scrollspyHeading4">
-              Previously Borrowed Books
-            </a>
-          </li>
-        </ul>
-      </nav>
+      {/* Navbar */}
       <div
         data-bs-spy="scroll"
         data-bs-target="#navbar-example2"
         data-bs-root-margin="0px 0px -40%"
         data-bs-smooth-scroll="true"
-        class="scrollspy-example bg-body-tertiary p-3 rounded-2"
-        tabindex="0"
+        className="scrollspy-example bg-body-tertiary p-3 rounded-2"
+        tabIndex="0"
       >
         <h4 id="scrollspyHeading1">Favourite Books</h4>
-        <div className="row py-2 g-5 my-2 justify-content-around">
+        <div className="row py-2 g-5 my-2">
           {/* {favBooks.map(shelfBooks)} */}
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                Author:None
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                Author:None
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
         <h4 id="scrollspyHeading2">Borrowed Books</h4>
-        <div className="row py-2 g-5 my-2 justify-content-around">
+        <div className="row py-2 g-5 my-2">
           {/* {borrowedBooks.map(fnBorrowedBooks)} */}
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                Author:None
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
         <h4 id="scrollspyHeading3">Requested Books</h4>
-        <div className="row py-2 g-5 my-2 justify-content-around">
+        <div className="row py-2 g-5 my-2">
           {/* {reqBooks.map(shelfBooks)} */}
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <h4 id="scrollspyHeading4">Previously Ordered Books</h4>
-        <div className="row py-2 g-5 my-2 justify-content-around">
-          {/* {reqBooks.map(shelfBooks)} */}
+        <h4 id="scrollspyHeading4">Previously Borrowed Books</h4>
+        <div className="row py-2 g-5 my-2">
+          {/* {prevBorrowedBooks.map(shelfBooks)} */}
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="card mb-3" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h5 className="card-title">Book Name:None</h5>
+                <p className="card-text">
+                  Author:None
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 }
+
