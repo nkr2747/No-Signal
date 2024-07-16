c22ce9c15cad1f0ed2c10cea1d2ced9c2fa0343f
nkr2747 <152243606+nkr2747@users.noreply.github.com>
2024-07-16 11:52:37 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -217,7 +217,18 @@ app.listen(PORT, () =>{
     console.log(`Server is runing at port no ${PORT}`);
 })
 
+//vercel deployment
+const allowedOrigins = ['https://no-signal-frontend.vercel.app'];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 // app.get('/', (req, res) =>
 // {
